c6248726ba5703e98c4ca851c64ae372d7e857d6
nathan-ribeiro <nathanribeiro778@gmail.com>
2025-02-10 21:10:04 -0300

    Refactor to only Rest with Mongo

--- src/main/java/com/communication/gRPC/GRpcApplication.java
+++ src/main/java/com/communication/gRPC/GRpcApplication.java
@@ -2,8 +2,9 @@ package com.communication.gRPC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "com.communication.gRPC")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GRpcApplication {
 
 	public static void main(String[] args) throws Exception {
